c55661694052fa57354528e09a2a48c5f2ec14a6
Ba348 <bdubielecki@gmail.com>
2023-04-29 23:21:49 +0200

    Creating CreateCommentReactionHandler

---
+++ src/Services/General/General.Application/Models/Parameters/CreateCommentReactionParameters.cs
@@ -0,0 +1,21 @@
+﻿using General.Application.Models.Enums;
+using Newtonsoft.Json;
+
+namespace General.Application.Models.Parameters;
+
+public class CreateCommentReactionParameters
+{
+    public int CommentId { get; init; }
+    public ReactionType Type { get; init; }
+
+    public CreateCommentReactionParameters()
+    {
+    }
+
+    [JsonConstructor]
+    public CreateCommentReactionParameters(int commentId, ReactionType type)
+    {
+        CommentId = commentId;
+        Type = type;
+    }
+}
