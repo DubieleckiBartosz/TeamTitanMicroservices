7d49d8090461a0e9e4d579e62c81a0e2aff86fa3
Ba348 <bdubielecki@gmail.com>
2023-04-29 23:28:31 +0200

    Creating DeleteCommentReactionHandler

---
+++ src/Services/General/General.Application/Features/Reaction/DeleteCommentReaction/DeleteCommentReactionCommand.cs
@@ -0,0 +1,13 @@
+﻿using General.Application.Models.Parameters;
+using MediatR;
+using Shared.Implementations.Abstractions;
+
+namespace General.Application.Features.Reaction.DeleteCommentReaction;
+
+public record DeleteCommentReactionCommand(int CommentId) : ICommand<Unit>
+{
+    public static DeleteCommentReactionCommand Create(DeleteCommentReactionParameters parameters)
+    {
+        return new DeleteCommentReactionCommand(parameters.CommentId);
+    }
+}
---
+++ src/Services/General/General.Application/Models/Parameters/DeleteCommentReactionParameters.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace General.Application.Models.Parameters;
+
+public class DeleteCommentReactionParameters
+{
+    public int CommentId { get; init; } 
+
+    public DeleteCommentReactionParameters()
+    {
+    }
+
+    [JsonConstructor]
+    public DeleteCommentReactionParameters(int commentId)
+    {
+        CommentId = commentId;
+    }
+}
