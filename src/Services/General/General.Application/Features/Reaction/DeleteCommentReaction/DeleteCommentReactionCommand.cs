41fd19f57070a6e6c9fbc02b348402004d1f971d
Ba348 <bdubielecki@gmail.com>
2023-04-29 23:41:53 +0200

    Creating DeletePostReactionHandler

--- src/Services/General/General.Application/Features/Reaction/DeleteCommentReaction/DeleteCommentReactionCommand.cs
+++ src/Services/General/General.Application/Features/Reaction/DeleteCommentReaction/DeleteCommentReactionCommand.cs
@@ -1,13 +1,6 @@
-﻿using General.Application.Models.Parameters;
-using MediatR;
+﻿using MediatR;
 using Shared.Implementations.Abstractions;
 
 namespace General.Application.Features.Reaction.DeleteCommentReaction;
 
-public record DeleteCommentReactionCommand(int CommentId) : ICommand<Unit>
-{
-    public static DeleteCommentReactionCommand Create(DeleteCommentReactionParameters parameters)
-    {
-        return new DeleteCommentReactionCommand(parameters.CommentId);
-    }
-}
+public record DeleteCommentReactionCommand(int CommentId) : ICommand<Unit>;
---
+++ src/Services/General/General.Application/Features/Reaction/DeletePostReaction/DeletePostReactionHandler.cs
@@ -0,0 +1,35 @@
+﻿using General.Application.Constants;
+using General.Application.Contracts;
+using MediatR;
+using Shared.Implementations.Abstractions;
+using Shared.Implementations.Core.Exceptions;
+using Shared.Implementations.Services;
+
+namespace General.Application.Features.Reaction.DeletePostReaction;
+
+public class DeletePostReactionHandler : ICommandHandler<DeletePostReactionCommand, Unit>
+{
+    private readonly IPostRepository _postRepository;
+    private readonly ICurrentUser _currentUser;
+
+    public DeletePostReactionHandler(IPostRepository postRepository, ICurrentUser currentUser)
+    {
+        _postRepository = postRepository ?? throw new ArgumentNullException(nameof(postRepository));
+        _currentUser = currentUser ?? throw new ArgumentNullException(nameof(currentUser));
+    }
+    public async Task<Unit> Handle(DeletePostReactionCommand request, CancellationToken cancellationToken)
+    {
+        var post = await _postRepository.GetPostWithReactions(request.PostId);
+        if (post == null)
+        {
+            throw new NotFoundException(ExceptionDetails.DetailsNotFound("GetPostWithReactions"),
+                ExceptionTitles.TitleNotFound("Post"));
+        }
+
+        post.RemoveReaction(_currentUser.UserId);
+        _postRepository.Update(post);
+        await _postRepository.SaveAsync();
+
+        return Unit.Value;
+    }
+}
