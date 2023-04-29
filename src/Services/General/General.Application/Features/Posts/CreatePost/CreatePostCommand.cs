8e1b0417f22487e7ac50be488c3554e1b7b8bc4a
Ba348 <bdubielecki@gmail.com>
2023-04-29 18:50:16 +0200

    Creating classes for the post operations

---
+++ src/Services/General/General.Application/Features/Posts/CreatePost/CreatePostCommand.cs
@@ -0,0 +1,7 @@
+﻿using Shared.Implementations.Abstractions;
+
+namespace General.Application.Features.Posts.CreatePost;
+
+public class CreatePostCommand : ICommand<int>
+{
+}
---
+++ src/Services/General/General.Application/Features/Posts/CreatePost/CreatePostHandler.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Implementations.Abstractions;
+
+namespace General.Application.Features.Posts.CreatePost;
+
+public class CreatePostHandler : ICommandHandler<CreatePostCommand, int>
+{
+    public Task<int> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
