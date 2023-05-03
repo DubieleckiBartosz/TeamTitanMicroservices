fd141fef5e891436585e7c67739d71a4b0c69248
Ba348 <bdubielecki@gmail.com>
2023-05-03 12:16:22 +0200

    Fixing during tests

--- Database/setupsql.sh
+++ Database/setupsql.sh
@@ -3,13 +3,13 @@
 
 #!/bin/bash
 
-SCRIPTS[0]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Identity-create-db.sql" 
-SCRIPTS[1]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanIdentity -i Identity-create-tables.sql" # -v VERBOSITY=1
-SCRIPTS[2]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanIdentity -i Identity-create-storedProcedures.sql"  
-SCRIPTS[3]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Calculator-create-db.sql" 
-SCRIPTS[4]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanCalculator -i Calculator-create-tables.sql" 
-SCRIPTS[5]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanCalculator -i Calculator-create-storedProcedures.sql" 
-SCRIPTS[6]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Background-create-db.sql" 
+SCRIPTS[0]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Background-create-db.sql" 
+SCRIPTS[1]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Identity-create-db.sql" 
+SCRIPTS[2]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanIdentity -i Identity-create-tables.sql" # -v VERBOSITY=1
+SCRIPTS[3]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanIdentity -i Identity-create-storedProcedures.sql"  
+SCRIPTS[4]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Calculator-create-db.sql" 
+SCRIPTS[5]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanCalculator -i Calculator-create-tables.sql" 
+SCRIPTS[6]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanCalculator -i Calculator-create-storedProcedures.sql" 
 SCRIPTS[7]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d master -i Management-create-db.sql" 
 SCRIPTS[8]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanManagement -i Management-create-tables.sql" 
 SCRIPTS[9]="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P sql123456(!) -d TeamTitanManagement -i Management-create-storedProcedures.sql" 
