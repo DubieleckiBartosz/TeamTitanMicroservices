9350e73d715afb5e077d65c7f32b4d120521b73f
Ba348 <bdubielecki@gmail.com>
2023-05-03 10:23:10 +0200

    Creating new post with attachments and fixing deleting files

---
+++ src/Services/Shared/Shared.Implementations/Tools/PathCreator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Shared.Implementations.Tools;
+
+public static class PathCreator
+{
+    public static string Create(this IFormFile formFile)
+    {
+        var path = $"File-{DateTime.Now.ToShortDateString()}-{Guid.NewGuid()}-{formFile.FileName}";
+        return path;
+    }
+
+    public static string CreateFilePath(this IFormFile formFile)
+    {
+        var fileName = Path.GetFileNameWithoutExtension(formFile.FileName);
+        var uniqueId = Guid.NewGuid().ToString("N");
+        var dateStr = DateTime.UtcNow.ToString("yyyy-MM-dd");
+
+        var filePath = $"{dateStr}-{uniqueId}-{fileName}{Path.GetExtension(formFile.FileName)}";
+        return filePath;
+    }
+}
